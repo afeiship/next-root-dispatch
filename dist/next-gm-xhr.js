4e592a79e601968966e4bbb93185839392f3d772
afeiship <1290657123@qq.com>
2020-11-04 10:54:39 +0800

    fix: xhr args bug

--- dist/next-gm-xhr.js
+++ dist/next-gm-xhr.js
@@ -2,8 +2,8 @@
  * name: @feizheng/next-gm-xhr
  * description: GM_xmlhttpRequest for next.
  * homepage: https://github.com/afeiship/next-gm-xhr
- * version: 1.0.5
- * date: 2020-11-04T01:49:59.528Z
+ * version: 1.0.6
+ * date: 2020-11-04T02:54:33.856Z
  * license: MIT
  */
 
