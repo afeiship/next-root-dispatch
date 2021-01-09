99740a1336273fd19abbb4d2cadaf602daf4fd0a
afeiship <1290657123@qq.com>
2021-01-09 16:24:52 +0800

    Release 1.0.2

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@
  * name: @jswork/next-gm-xhr
  * description: GM_xmlhttpRequest for next.
  * homepage: https://github.com/afeiship/next-gm-xhr
- * version: 1.0.1
- * date: 2021-01-06 22:22:38
+ * version: 1.0.2
+ * date: 2021-01-09 16:24:47
  * license: MIT
  */
 
